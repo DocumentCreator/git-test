ff72121eb479215d4dc21cfcc434fb835f1beb26
houkailiang <1176040867@qq.com>
2020-09-25 06:19:18 +0800

    修改git-test-project01的FirstClass类

---
+++ git-test-project01/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itheima.git;/*
+ *
+ *@author   houkailiang
+ *@date   2020/9/25  - 6:00
+ */
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+    }
+
+    public void teset(){
+        int i = 50;
+    }
+}
